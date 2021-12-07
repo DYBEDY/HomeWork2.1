242e29c7dcca88c7056bf213fce2d713295746c2
DYBEDY <rd.iosdeveloper@gmail.com>
2021-12-07 18:27:03 +0300

    Traffic Lights

--- HomeWork2.1/ViewController.swift
+++ HomeWork2.1/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CurrentLight {
+    case red
+    case yellow
+    case green
+}
 
+class ViewController: UIViewController {
+    @IBOutlet var redColorView: UIView!
+    @IBOutlet var yellowColorView: UIView!
+    @IBOutlet var greenColorView: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    var currentLight: CurrentLight = .red
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redColorView.layer.cornerRadius = redColorView.frame.size.width / 2
+        yellowColorView.layer.cornerRadius = yellowColorView.frame.size.width / 2
+        greenColorView.layer.cornerRadius = greenColorView.frame.size.width / 2
+        startButton.layer.cornerRadius = 20
+        
+        redColorView.alpha = 0.4
+        yellowColorView.alpha = 0.4
+        greenColorView.alpha = 0.4
     }
 
-
+    @IBAction func startPressed() {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            currentLight = .yellow
+            redColorView.alpha = 1
+            yellowColorView.alpha = 0.4
+            greenColorView.alpha = 0.4
+        case .yellow:
+            currentLight = .green
+            yellowColorView.alpha = 1
+            redColorView.alpha = 0.4
+            greenColorView.alpha = 0.4
+        case .green:
+            currentLight = .red
+            greenColorView.alpha = 1
+            yellowColorView.alpha = 0.4
+            redColorView.alpha = 0.4
+        }
+    }
+    
 }
 
